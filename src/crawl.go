ea67c40a6cd1529da8e2fd6d09b32cc055436d55
diljith <diljithisonlinenow@gmail.com>
2020-02-26 20:52:45 +1300

    crawl update

--- src/crawl.go
+++ src/crawl.go
@@ -1,16 +1,37 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"io/ioutil"
 	"net/http"
 	"net/url"
+	"os"
+	"runtime"
 	"strings"
 
+	"github.com/fatih/color"
 	"github.com/gocolly/colly"
 )
 
 func main() {
+
+	options := bufio.NewReader(os.Stdin)
+	gr := color.New(color.FgHiGreen, color.Bold)
+	finflag := make(chan string)
+	gr.Printf("Enter URL  : ")
+	urlval, _ := options.ReadString('\n')
+	urlval = removenewline(urlval)
+	go crawlforms(urlval, finflag)
+	<-finflag
+	go crawllinks(urlval, finflag)
+	<-finflag
+
+}
+
+func crawlforms(urlval string, finflag chan string) {
+	ylw := color.New(color.FgHiYellow, color.Bold)
+	wht := color.New(color.FgHiWhite, color.Bold)
 
 	c := colly.NewCollector(
 		colly.UserAgent("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.246"),
@@ -21,56 +42,55 @@ func main() {
 		id := e.Attr("id")
 		method := e.Attr("method")
 
-		fmt.Println("Action : " + action)
-		fmt.Println("Form ID : " + id)
-		fmt.Println("Method : " + method)
-
+		ylw.Println("Action : " + action)
+		ylw.Println("Form ID : " + id)
+		ylw.Println("Method : " + method)
+		wht.Println("---------------------------------------------------")
 		e.ForEach("input", func(index int, element *colly.HTMLElement) {
 			if element.Attr("type") == "text" || element.Attr("type") == "password" {
-				fmt.Println("Name ==> " + element.Attr("name") + " [Type : " + element.Attr("type") + "]")
-				fmt.Println(element.Request.URL.String())
+				ylw.Println("Name ==> " + element.Attr("name") + " [Type : " + element.Attr("type") + "]")
+				wht.Println(element.Request.URL.String())
 			}
 		})
+		wht.Println("---------------------------------------------------")
+
 	})
-	/*err := c.Post("https://www.hackthissite.org/user/login", map[string]string{"username": "admin", "password": "admin"})
-	if err != nil {
-		fmt.Println(err)
-	}
-	c.OnResponse(func(r *colly.Response) {
-		fmt.Println("response received", string(r.Body))
-	})*/
+	c.Visit(urlval)
+	finflag <- "finished"
+}
+func crawllinks(urlval string, finflag chan string) {
+	bl := color.New(color.FgHiBlue, color.Bold)
 
-	/*c.OnHTML("a[href]", func(e *colly.HTMLElement) {
+	c := colly.NewCollector(
+		colly.UserAgent("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.246"),
+	)
 
-		// Print link
-		fmt.Printf("Link found: %q -> %s\n", e.Request.AbsoluteURL(e.Attr("href")), e.Attr("href"))
+	c.OnHTML("a[href]", func(e *colly.HTMLElement) {
 
-		//fmt.Printf(link)
+		bl.Printf("Link : %q \n", e.Request.AbsoluteURL(e.Attr("href")))
 
-		// Visit link found on page
-		// Only those links are visited which are in AllowedDomains
-		//c.Visit(e.Request.AbsoluteURL(link))
-	})*/
+	})
+	c.Visit(urlval)
+	finflag <- "finished"
 
-	// Before making a request print "Visiting ..."
-	/*c.OnRequest(func(r *colly.Request) {
-		fmt.Println("Visiting", r.URL.String())
-	})*/
-
-	// Start scraping on https://hackerspaces.org
-	c.Visit("https://www.hackthissite.org")
-	//c2.Visit("https://www.hackthissite.org")
-
-	//link := e.Request.AbsoluteURL(e.Attr("href"))
 }
 
-func formsubmissions() {
+func removenewline(val string) string {
+	if runtime.GOOS == "linux" {
+		val = strings.Replace(val, "\n", "", -1)
+	} else {
+		val = strings.TrimSuffix(val, "\r\n")
+	}
+	return val
+}
+
+func formsubmissions(urlval string) {
 	formdata := url.Values{}
 	formdata.Set("username", "admin")
 	formdata.Set("password", "admin")
 
 	client := &http.Client{}
-	req, _ := http.NewRequest("POST", "https://www.hackthissite.org/user/login", strings.NewReader(formdata.Encode()))
+	req, _ := http.NewRequest("POST", urlval, strings.NewReader(formdata.Encode()))
 	req.Header.Add("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.246")
 	req.Header.Add("Referer", "https://www.hackthissite.org")
 	resp, err := client.Do(req)
